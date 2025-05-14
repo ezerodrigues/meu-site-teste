55812ffa79fa75756213f09f912a4064818de466
Eliézer Rodrigues <59882187+ezerodrigues@users.noreply.github.com>
2025-05-14 14:05:57 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function sobre () {
+    window.location.replace('index.html');
+}
+
+function projetos () {
+    window.location.replace('projetos.html');
+}
+
+function contatos () {
+    window.location.replace('contatos.html');
+}
