173edc9cd55c26a1bdf88ab4ce96e2acd89b544b
ezerodrigues <ezerodrigues@msn.com>
2025-05-14 14:53:08 -0300

    Sua mensagem de alteração

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 function sobre () {
     window.location.replace('index.html');
 }
@@ -9,3 +10,16 @@ function projetos () {
 function contatos () {
     window.location.replace('contatos.html');
 }
+=======
+function sobre () {
+    window.location.replace('index.html');
+}
+
+function projetos () {
+    window.location.replace('projetos.html');
+}
+
+function contatos () {
+    window.location.replace('contatos.html');
+}
+>>>>>>> debe37c7caf3843e2d13fe917bb4b9fff88242b9
