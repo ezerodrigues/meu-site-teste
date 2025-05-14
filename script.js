26616abd9460603cebae7cf31d68b88eaa72fbaa
ezerodrigues <ezerodrigues@msn.com>
2025-05-14 14:58:33 -0300

    Correção script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function sobre () {
     window.location.replace('index.html');
 }
@@ -10,16 +9,3 @@ function projetos () {
 function contatos () {
     window.location.replace('contatos.html');
 }
-=======
-function sobre () {
-    window.location.replace('index.html');
-}
-
-function projetos () {
-    window.location.replace('projetos.html');
-}
-
-function contatos () {
-    window.location.replace('contatos.html');
-}
->>>>>>> debe37c7caf3843e2d13fe917bb4b9fff88242b9
